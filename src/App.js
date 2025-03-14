eaf0af2a25246f87cdb1a1d29307aa83d10ed7d3
Itor Carlos <itor51carlos@gmail.com>
2025-03-14 14:47:38 -0300

    add rota pra edicao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { SideBar } from "./components/SideBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CadastrarArea } from "./pages/area/CadastrarArea";
 import { ListarArea } from "./pages/area/ListarArea";
+import { CadastrarEditarArea } from "./pages/area/CadastrarEditarArea";
 import './index.css'
 
 function App() {
@@ -13,7 +13,8 @@ function App() {
       <div className="main-content">
           <div>
               <Routes>
-                <Route path="/area/cadastrar_area" element={<CadastrarArea/>}/>
+                <Route path="/area/cadastrar_area/" element={<CadastrarEditarArea/>}/>
+                <Route path="/area/editar/:id" element={<CadastrarEditarArea />} />
                 <Route path="/area/listar_area" element={<ListarArea/>}/>
               </Routes>
           </div>
