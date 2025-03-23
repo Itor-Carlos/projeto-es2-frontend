6a136b6e72b2f5bc61a9e280e9db780375c57c5e
DaviFontes <davisouza.dsfs@gmail.com>
2025-03-23 14:13:42 -0300

    corrigida escrita de razaosocial

--- src/pages/fornecedor/CadastrarEditarFornecedor.js
+++ src/pages/fornecedor/CadastrarEditarFornecedor.js
@@ -26,7 +26,7 @@ export const CadastrarEditarFornecedor = () => {
         cep: "",
         documento: "",
         telefone: "",
-        razaoSocial: "",
+        razaosocial: "",
         empresa: "",
     });
 
