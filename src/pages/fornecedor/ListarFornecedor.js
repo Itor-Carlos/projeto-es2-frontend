7559d80a5ab1aab289ced774cab4600f394e1845
heitor santos <heitor-081@outlook.com>
2025-03-26 18:44:02 -0300

    FEAT: modulo de listagem dos fornecedores

--- src/pages/fornecedor/ListarFornecedor.js
+++ src/pages/fornecedor/ListarFornecedor.js
@@ -3,5 +3,41 @@ import { TitleSection } from "../../components/TitleSection";
 import { TopBar } from "../../components/TopBar";
 
 export const ListarFornecedor = () => {
-    
-}
+    const headers = [
+        {
+            name: "idfornecedor",
+            label: "ID",
+            type: "number"
+        },
+        {
+            name: "nome",
+            label: "Nome",
+            type: "string"
+        },
+        {
+            name: "email",
+            label: "Email",
+            type: "string"
+        },
+        {
+            name: "telefone",
+            label: "Telefone",
+            type: "string"
+        },
+        {
+            name: "documento",
+            label: "Documento",
+            type: "document"
+        }
+    ];
+
+    return (
+        <>
+            <TopBar entity={"Fornecedor"} useCase={"Listar Fornecedores"} textButton={"Novo Fornecedor"} redirectRoute={"/fornecedor/cadastrar"} />
+            <div className="card">
+                <TitleSection title={"Listar Fornecedores"} />
+                <List entity={"fornecedor"} headers={headers} baseUrl={"http://localhost:3306/fornecedores"} />
+            </div>
+        </>
+    );
+};
